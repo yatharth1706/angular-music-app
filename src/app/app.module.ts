6a2f03ed9bae603db4ebd025ad374cd0e18a5d42
unknown <yatharthverma070@gmail.com>
2019-12-28 14:41:12 +0530

    Refactored code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { ViewComponent } from './view/view.component';
-import { CreateAccountPipe } from './create-account.pipe';
+// import { CreateAccountPipe } from './create-account.pipe';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { FooterComponent } from './footer/footer.component';
 import { SafePipe } from './safe.pipe';
 import { LoginComponent } from './login/login.component';
 import { AudioComponent } from './audio/audio.component';
@@ -21,13 +21,10 @@ import { AudioComponent } from './audio/audio.component';
     AppComponent,
     SignupComponent,
     ViewComponent,
-    CreateAccountPipe,
     HomeComponent,
-    NavbarComponent,
-    FooterComponent,
-    SafePipe,
     LoginComponent,
-    AudioComponent
+    AudioComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
--- src/app/short.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortPipe } from './short.pipe';
-
-describe('ShortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
